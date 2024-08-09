792e260cbcfebd302238b370fc23cc65b283a21b
valyastriz <valyastriz@gmail.com>
2024-08-09 15:08:25 -0400

    update server.js to use environment variables for session secret

--- server.js
+++ server.js
@@ -12,14 +12,19 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sess = {
-  secret: 'Super secret secret',
-  cookie: {},
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize,
-  }),
-};
+    secret: process.env.SESSION_SECRET,
+    cookie: {
+      maxAge: 300000,
+      httpOnly: true,
+      secure: false,
+      sameSite: 'strict',
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+  };
 
 app.use(session(sess));
 
