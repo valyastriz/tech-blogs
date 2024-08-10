62a8dbdf19e636218b23d06bcf621f4cb7f062c5
valyastriz <valyastriz@gmail.com>
2024-08-10 14:02:12 -0400

    created commentRoutes.js, uncommented necessary lines of code in index.js for it be able to run

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,5 +1,92 @@
 //Routes related to comments on posts
 
+const express = require('express');
 const router = require('express').Router();
+const { Comment } = require('../../models');
+
+// get all comments
+router.get('/', async (req, res) => {
+    try {
+        const comments = await Comment.findAll();
+        res.json(comments);
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// get a comment by id
+router.get('/:id', async (req, res) => {
+    try {
+        const comment = await Comment.findByPk(req.params.id);
+
+        if (!comment) {
+            return res.status(404).json({ message: 'No comment found with this id' });
+        }
+
+        res.json(comment);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// create a new comment
+router.post('/', async (req, res) => {
+    try {
+        const commentData = await Comment.create(req.body);
+
+        req.session.save(() => {
+            // req.session.user_id = userData.id;
+            // req.session.logged_in = true;
+
+            res.status(200).json(commentData);
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// update a comment by id
+router.put('/:id', async (req, res) => {
+    try {
+        const updatedComment = await Comment.update(req.body, {
+            where: {
+                id: req.params.id
+            }
+        });
+
+        if (!updatedComment[0]) { //checking if any rows were affected
+            res.status(404).json({ message: 'No comment found with this id '});
+            return;
+        }
+
+        res.status(200).json(updatedComment);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// delete comment by id
+router.delete('/:id', async (req, res) => {
+    try {
+
+        const deletedComment = await Comment.destroy({
+            where: {
+                id: req.params.id
+            }
+        });
+
+        if (!deletedComment) {
+            res.status(404).json({ message: 'No comment found with this id '});
+            return;
+        }
+
+        res.status(200).json({ message: 'Comment deleted' });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json(err);
+    }
+});
+
 
 module.exports = router;
