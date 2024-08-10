933d76af4207bc761e493b6bb340e854d7ca8b98
valyastriz <valyastriz@gmail.com>
2024-08-10 14:41:09 -0400

    added html to man.handlesbars and home.handlebars as well as some css styling. added to ohmeRoute.js to handle routing to url

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,9 @@
 // Routes related to the homepage and general navigation
 const router = require('express').Router();
 
-
+// Route to render the homepage
+router.get('/', (req, res) => {
+    res.render('home'); // 'home' corresponds to 'home.handlebars' in your views folder
+});
 
 module.exports = router;
