7a61b48c389f8466148e20b39bcb500a3894a59a
valyastriz <valyastriz@gmail.com>
2024-08-09 22:30:22 -0400

    added api to create a new user in userRoutes.js file with authentication portion commented out

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -15,20 +15,20 @@ router.get('/', async (req, res) => {
 });
 
 // create a new user
-// router.post('/', async (req, res) => {
-//     try {
-//         const userData = await User.create(req.body);
+router.post('/', async (req, res) => {
+    try {
+        const userData = await User.create(req.body);
 
-//         req.session.save(() => {
-//             req.session.user_id;
-//             req.session.logged_in = true;
+        req.session.save(() => {
+            // req.session.user_id = userData.id;
+            // req.session.logged_in = true;
 
-//             res.status(200).json(userData);
-//         });
-//     } catch (err) {
-//         res.status(400).json(err);
-//     }
-// });
+            res.status(200).json(userData);
+        });
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
 
 
 
