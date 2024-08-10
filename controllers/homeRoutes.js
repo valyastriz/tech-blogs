102424ffc8b987d595aac29378d90598f093b1bc
valyastriz <valyastriz@gmail.com>
2024-08-10 14:50:02 -0400

    removed undeed footer and header partials since they will be embedded in main.handlebars. Added data to homeRoutes.js to render posts

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,8 +2,19 @@
 const router = require('express').Router();
 
 // Route to render the homepage
-router.get('/', (req, res) => {
-    res.render('home'); // 'home' corresponds to 'home.handlebars' in your views folder
+router.get('/', async (req, res) => {
+    try {
+        // fetch all posts from the database
+        const postData = await Post.findAll();
+
+        // serialize data so the template can read it
+        const posts = postData.map((post) => post.get({ plain: true }));
+
+        // pass posts to the homepage template
+        res.render('home', { posts });
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -3,9 +3,6 @@ const homeRoutes = require('./homeRoutes');
 const dashboardRoutes = require('./dashboardRoutes');
 const apiRoutes = require('./api');
 
-console.log(typeof homeRoutes);  // Should output 'function'
-console.log(typeof dashboardRoutes);  // Should output 'function'
-console.log(typeof apiRoutes);  // Should output 'function'
 router.use('/', homeRoutes);
 router.use('/dashboard', dashboardRoutes);
 router.use('/api', apiRoutes);
