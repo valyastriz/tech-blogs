17b857fac14d3dde53b5c3a22a486fe5c8548dab
valyastriz <valyastriz@gmail.com>
2024-08-12 17:15:57 -0400

    added dasboard rendering route to homeRoutes.js and dashboard.handlebars page, working on degubbing

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -1,7 +1,26 @@
-// Routes related to the user dashboard
-
 const router = require('express').Router();
+const { Post } = require('../models');
+const withAuth = require('../utils/auth');
 
+router.get('/', withAuth, async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+            where: { user_id: req.session.user_id },
+            attributes: ['id', 'title', 'content'],
+            order: [['created_at', 'DESC']],
+        });
 
+        const posts = postData.map(post => post.get({ plain: true }));
+        console.log(posts); 
+        
+        res.render('dashboard', {
+            posts,
+            logged_in: req.session.logged_in,
+        });
+    } catch (err) {
+        console.error(err); 
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
