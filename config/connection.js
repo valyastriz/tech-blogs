800e147a55dc31d701ad417dca52fd26de2fde2d
valyastriz <valyastriz@gmail.com>
2024-08-12 22:53:28 -0400

    updated connection.js to require ssl

--- config/connection.js
+++ config/connection.js
@@ -2,15 +2,31 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 const sequelize = process.env.DB_URL
-  ? new Sequelize(process.env.DB_URL)
-  : new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: 'process.env.DB_HOST',
+  ? new Sequelize(process.env.DB_URL, {
       dialect: 'postgres',
-    }
-  );
+      protocol: 'postgres',
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false, // Set to true if your database requires full certificate verification
+        },
+      },
+    })
+  : new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: process.env.DB_HOST,
+        dialect: 'postgres',
+        port: process.env.DB_PORT,
+        dialectOptions: {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false,
+          },
+        },
+      }
+    );
 
 module.exports = sequelize;
