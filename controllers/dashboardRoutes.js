5fabe5f278589eba90762c171c22d45922d3008e
valyastriz <valyastriz@gmail.com>
2024-08-12 23:08:38 -0400

    added console.log for debugging live site

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -3,6 +3,7 @@ const { Post } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', withAuth, async (req, res) => {
+    console.log(req.session);  
     try {
         const postData = await Post.findAll({
             where: { user_id: req.session.user.id },
