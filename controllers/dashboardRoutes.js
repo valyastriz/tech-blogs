1c1829b19da5e8b2650475192e73de7178e11003
valyastriz <valyastriz@gmail.com>
2024-08-12 22:49:49 -0400

    updated connection to point to env host

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -2,17 +2,17 @@ const router = require('express').Router();
 const { Post } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/', withAuth, async (req, res) => {
+router.get('/',/* withAuth,*/ async (req, res) => {
     try {
         const postData = await Post.findAll({
-            where: { user_id: req.session.user_id },
+            where: { user_id: req.session.user.id },
             attributes: ['id', 'title', 'content'],
             order: [['created_at', 'DESC']],
         });
 
         const posts = postData.map(post => post.get({ plain: true }));
         console.log(posts); 
-        
+
         res.render('dashboard', {
             posts,
             logged_in: req.session.logged_in,
